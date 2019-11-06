e6581a2d63ec3d80aaacf5bf95e17d8dfc3f1ce6
svupper <56261963+svupper@users.noreply.github.com>
2019-11-06 22:51:40 +0100

    Update NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,24 +1,32 @@
 import numpy as np
+import math as m
 
-X=np.random.randn(0,10)
-W=np.random.randn(0,1)*0.01
+X=np.random.randn(9,1)
+X=np.concatenate[X,1]
+Y=np.random.random_sample
+W=np.random.randn(10,1)*0.01    # +bias
 
+v=np.dot(W,X)
 
 
 loss=lambda y,yd:(1/2)*(y-yd)
-sig=lambda v:(exp(v)-exp(-v))/(exp(v)-exp(-v))
+sig=lambda v:(m.exp(v)-m.exp(-v))/(m.exp(v)-m.exp(-v))
+sigp=lambda v:sig(v)(1-sig(v))
 
-
+#dx=dx*dv #dL gradient incoming, dv gradient of the gate, dx gradient outputting
+y=sig(v)
 
 act={}
 
-
-for i in range(2)
-	y=v(W*X)
-	
-	
-	
-for i=2 in range(inv(2))
-	dL=1
-	dx=dL*dv
-	
+##
+#
+#for i in range(size(W(1,1,:))):
+#	y=v(W*X)
+#	
+#	
+#
+#for i in X:
+#	dL=1
+#	dx=dL*dv
+#    */
+##
