851ce301b637ca15a3cb32b8e056672bd201817c
svupper <56261963+svupper@users.noreply.github.com>
2019-11-06 15:42:48 +0100

    Create NeuralNetwork.py

---
+++ NeuralNetwork.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+X=np.random.randn(0,10)
+W=np.random.randn(0,1)*0.01
+
+
+
+loss=lambda y,yd:(1/2)*(y-yd)
+sig=lambda v:(exp(v)-exp(-v))/(exp(v)-exp(-v))
+
+
+
+act={}
+
+
+for i in range(2)
+	y=v(W*X)
+	
+	
+	
+for i=2 in range(inv(2))
+	dL=1
+	dx=dL*dv
+	
